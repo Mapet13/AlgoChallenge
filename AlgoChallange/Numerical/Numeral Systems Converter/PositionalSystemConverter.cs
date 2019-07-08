680da0a29212f834526cb7558f8f4b6137948554
Mapet13 <jakubsordyl1@gmail.com>
2019-07-08 16:45:25 +0200

    Added special conditions to PositionalSystemConverter

--- AlgoChallange/Numerical/Numeral Systems Converter/PositionalSystemConverter.cs
+++ AlgoChallange/Numerical/Numeral Systems Converter/PositionalSystemConverter.cs
@@ -2,6 +2,9 @@
 
 namespace AlgoChallange.Numerical
 {
+    /// <summary>
+    /// Supports system whose base is an integer from 2 to 16
+    /// </summary>
     public class PositionalSystemConverter
     {
         public string Convert(int number, int outBase)
@@ -16,7 +19,16 @@ namespace AlgoChallange.Numerical
 
         public string Convert(string number, int inBase, int outBase)
         {
+            if (inBase == outBase) return number;
+            if (number == "0") return "0";
+            if (!AreArgumentsCorrect(inBase, outBase)) throw new ArgumentException("Unsuported base");
+
             return "0";
+        }
+
+        private static bool AreArgumentsCorrect(int inBase, int outBase)
+        {
+            return inBase < 2 || outBase < 2 || inBase > 16 || outBase > 16;
         }
     }
 }
