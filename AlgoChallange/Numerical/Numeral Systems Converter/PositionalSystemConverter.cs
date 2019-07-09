1ed9f3ea1877349220c09cbdf55d4e59b876d28b
Mapet13 <jakubsordyl1@gmail.com>
2019-07-09 10:13:23 +0200

    Added converter from any positional system to decimal

--- AlgoChallange/Numerical/Numeral Systems Converter/PositionalSystemConverter.cs
+++ AlgoChallange/Numerical/Numeral Systems Converter/PositionalSystemConverter.cs
@@ -7,28 +7,93 @@ namespace AlgoChallange.Numerical
     /// </summary>
     public class PositionalSystemConverter
     {
+        /// <summary>
+        /// Convert from decimal to any system
+        /// </summary>
         public string Convert(int number, int outBase)
         {
-            return Convert(number.ToString(), 10, outBase);
+            if (AreArgumentCorrect(outBase)) throw new ArgumentException(); //so many same throws
+            return FromDecimal(number, outBase);
         }
 
+        /// <summary>
+        /// Convert from any system to decimal
+        /// </summary>
         public int Convert(string number, int inBase)
         {
-            return int.Parse(Convert(number, inBase, 10));
+            if (AreArgumentCorrect(inBase)) throw new ArgumentException();
+            return ToDecimal(number, inBase);
         }
 
+        /// <summary>
+        /// Convert from any system to any system
+        /// </summary>
         public string Convert(string number, int inBase, int outBase)
         {
             if (inBase == outBase) return number;
+            number = DeleteNonsignificantZeroes(number);
             if (number == "0") return "0";
-            if (!AreArgumentsCorrect(inBase, outBase)) throw new ArgumentException("Unsuported base");
+            if (AreArgumentsCorrect(inBase, outBase)) throw new ArgumentException();
 
-            return "0";
+            return FromDecimal(ToDecimal(number, inBase), outBase);
         }
 
-        private static bool AreArgumentsCorrect(int inBase, int outBase)
+        private int ToDecimal(string number, int inBase) //How wto clean it???
         {
-            return inBase < 2 || outBase < 2 || inBase > 16 || outBase > 16;
+            if (inBase == 10) return int.Parse(number);
+            number = DeleteNonsignificantZeroes(number);
+            if (number == "0") return 0;
+
+            int result = 0;
+            int length = number.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                result += Pow(inBase, length - 1 - i) * signs.IndexOf(number[i]);
+            }
+
+            return result;
         }
+
+        private string DeleteNonsignificantZeroes(string number)
+        {
+            while (number.StartsWith("0") && number.Length > 1)
+            {
+                number = number.Substring(1);
+            }
+
+            return number;
+        }
+
+        private string FromDecimal(int number, int outBase)
+        {
+            if (outBase == 10) return number.ToString();
+            if (number == 0) return "0";
+
+            return number.ToString(); ;
+        }
+
+        private bool AreArgumentsCorrect(int inBase, int outBase)
+        {
+            return AreArgumentCorrect(inBase) || AreArgumentCorrect(outBase);
+        }
+        private bool AreArgumentCorrect(int arg)
+        {
+            return arg < 2 || arg > 16;
+        }
+
+        public int Pow(int num, int power)
+        {
+            if (power < 0) throw new ArgumentException("Negative powers are not supported");
+            if (power == 0) return 1;
+            if (power % 2 == 0)
+            {
+                int val = Pow(num, power / 2);
+                return val * val;
+            }
+            return num * Pow(num, power - 1);
+        }
+
+        private const string signs = "0123456789ABCDEF";
+
     }
 }
