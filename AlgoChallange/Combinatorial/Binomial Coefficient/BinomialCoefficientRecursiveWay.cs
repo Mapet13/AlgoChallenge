ba3e19862f8f879434b650838868fc9da5c7eb0d
Mapet13 <jakubsordyl1@gmail.com>
2019-07-10 14:59:46 +0200

    Added Recursive way to calculate binomial coefficient

---
+++ AlgoChallange/Combinatorial/Binomial Coefficient/BinomialCoefficientRecursiveWay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoChallange.Combinatorial
+{
+    public class BinomialCoefficientRecursiveWay : IBinomialCoefficientCalculator
+    {
+        public ulong Calculate(uint n, uint k)
+        {
+            if (n < k) throw new ArgumentException("N can't be less than K");
+            if (k == 0 || k == n || n == 1) return 1;
+
+            return Calculate(n - 1, k - 1) + Calculate(n - 1, k);
+        }
+    }
+}
---
+++ AlgoTest/Combinatorial/Binomial Coefficient/BinomialCoefficientRecursiveWayTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgoChallange.Combinatorial;
+
+namespace AlgoTest.Combinatorial.BinomialCoefficient
+{
+    [TestClass]
+    public class BinomialCoefficientRecursiveWayTest
+        : BinomialCoefficientTest<BinomialCoefficientRecursiveWay>
+    {
+
+    }
+}
